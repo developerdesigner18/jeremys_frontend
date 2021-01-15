134c93c21597a1391d17a8da129f68a07a88f2f8
developer.designer18 <developer.designer18@gmail.com>
2021-01-15 15:57:54 +0530

    tested api integration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import Login from './components/login/Login';
+import UserProfile from './components/profile/UserProfile';
 import Register from './components/register/Register';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/profile" component={UserProfile} />
       </BrowserRouter>
     </div>
   );
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,56 +1,93 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import '../../assets/css/signin.css';
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
-function Login() {
-    const history = useHistory();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+function Login(props) {
+  const history = useHistory();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const goToRegister = () => {
-        history.push('/register');
+  const goToRegister = () => {
+    history.push('/register');
+  };
+
+  const goToLogin = () => {
+    history.push('/login');
+  };
+
+  const callLogin = e => {
+    console.log('-=-=- fn called');
+    e.preventDefault();
+
+    if (email && password) {
+      const dataToPass = {
+        email,
+        password,
+      };
+      axios
+        .post(
+          'https://radiant-beach-45888.herokuapp.com/auth/signin',
+          dataToPass
+        )
+        .then(result => {
+          console.log('result ', result.data);
+          if (result.status === 200) {
+            props.history.push('/profile', {
+              name: result.data.data.firstName,
+            });
+            localStorage.setItem('token', result.data.authToken);
+            localStorage.setItem('name', result.data.data.firstName);
+          }
+        })
+        .catch(err => alert(err.toString()));
     }
+  };
 
-    const goToLogin = () => {
-        history.push('/login');
-    }
-
-    const callLogin = (e) => {
-        e.preventDefault();
-
-        if (email && password) {
-            axios.post('https://radiant-beach-45888.herokuapp.com/auth/signin')
-            .then((result) => {
-                console.log('result ', result);
-                alert('login done');
-            })
-            .catch((err) => console.log('error in login ', err));
-        }
-    }
-
-    return (
-        <div className="container">
-            <form method='post' onSubmit={(e) => callLogin(e)}>
-                <div className="form_container p-3 p-md-5 mt-5">
-                    <div className="form_detail">
-                        <label>E-mail</label>
-                        <input type="email" name="email" required onChange={(e) => setEmail(e.target.value)} />
-                    </div>
-                    <div className="form_detail">
-                        <label>PASSWORD</label>
-                        <input type="password" name="password" required onChange={(e) => setPassword(e.target.value)} />
-                    </div>
-                    <div className="forgot_pass"><a><span>forgot password</span></a></div>
-                    <div className="remember">
-                        <input type="checkbox" name="" /><label>Remember me</label>
-                    </div>
-                    <div className="login"><button>Login</button></div>
-                    <div className="text-center newbie">are You a newbie? <a onClick={goToRegister} style={{ cursor:"pointer" }}>Sign Up</a></div>
-                </div>
-            </form>
+  return (
+    <div className="container">
+      <form method="post" onSubmit={e => callLogin(e)}>
+        <div className="form_container p-3 p-md-5 mt-5">
+          <div className="form_detail">
+            <label>E-mail</label>
+            <input
+              type="email"
+              name="email"
+              required
+              onChange={e => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form_detail">
+            <label>PASSWORD</label>
+            <input
+              type="password"
+              name="password"
+              required
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="forgot_pass">
+            <a>
+              <span>forgot password</span>
+            </a>
+          </div>
+          <div className="remember">
+            <input type="checkbox" name="" />
+            <label>Remember me</label>
+          </div>
+          <div className="login">
+            <button>Login</button>
+          </div>
+          <div className="text-center newbie">
+            are You a newbie?{' '}
+            <a onClick={goToRegister} style={{ cursor: 'pointer' }}>
+              Sign Up
+            </a>
+          </div>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
