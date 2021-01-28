48efd4def5cdabb0d60f1f49a3013411e49c53f5
developer.designer18@ <developer.designer18@gmail.com>
2021-01-28 10:06:14 +0530

    added artists profile page and resolved conflicts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 import { BrowserRouter, Route } from "react-router-dom";
-import artistsProfile from "./components/artistsProfile/artistsProfile";
+import ArtistsProfile from "./components/artistsProfile/artistsProfile";
+import CustomerService from "./components/CustomerService";
 import fanHomePage from "./components/fanHomePage/fanHomePage";
 import ForgotPassword from "./components/ForgotPassword";
 import HomePage from "./components/HomePage";
@@ -9,21 +9,8 @@ import UserProfile from "./components/profile/UserProfile";
 import UserProfileCopy from "./components/profile/UserProfile copy";
 import Register from "./components/register/Register";
 import ResetPassword from "./components/Reset";
+import TermsCondition from "./components/TermsCondition";
 import UserCategoryHomePage from "./components/userCategoryHome/UserCategoryHomePage";
-=======
-import { BrowserRouter, Route } from "react-router-dom"
-import CustomerService from "./components/CustomerService"
-import fanHomePage from "./components/fanHomePage/fanHomePage"
-import ForgotPassword from "./components/ForgotPassword"
-import HomePage from "./components/HomePage"
-import Login from "./components/login/Login"
-import UserProfile from "./components/profile/UserProfile"
-import UserProfileCopy from "./components/profile/UserProfile copy"
-import Register from "./components/register/Register"
-import ResetPassword from "./components/Reset"
-import TermsCondition from "./components/TermsCondition"
-import UserCategoryHomePage from "./components/userCategoryHome/UserCategoryHomePage"
->>>>>>> 2bcab9f1aadffa097dfe82adf65e5a05a22055fd
 
 function App() {
   return (
@@ -37,13 +24,13 @@ function App() {
         <Route exact path="/fanHomePage" component={fanHomePage} />
         <Route exact path="/forgotPassword" component={ForgotPassword} />
         <Route exact path="/resetPassword" component={ResetPassword} />
-        <Route exact path="/artistProfile" component={artistsProfile} />
+        <Route exact path="/artistProfile" component={ArtistsProfile} />
         <Route exact path="/userHomepage" component={UserCategoryHomePage} />
         <Route exact path="/termsCondition" component={TermsCondition} />
         <Route exact path="/customerService" component={CustomerService} />
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
