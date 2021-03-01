bd86a50c7359118fc84e7473e41d6692766b8c45
developer.designer18 <developer.designer18@gmail.com>
2021-03-01 11:21:08 +0530

    change backend base url in env

--- src/socketIO.js
+++ src/socketIO.js
@@ -1,6 +1,12 @@
 import socketIOClient from "socket.io-client";
 
-export const socket = socketIOClient("http://54.236.46.101:8000", {
+export const socket = socketIOClient("https://artsiam.com:8000", {
   jsonp: false,
   transports: ["websocket"], // you need to explicitly tell it to use websockets
+  reconnection: true,
+  reconnectionDelay: 1000,
+  reconnectionDelayMax: 5000,
+  reconnectionAttempts: Infinity,
 });
+
+socket.on("connect", () => console.log("socket connection establish"));
